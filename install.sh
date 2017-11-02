93a12bbe370d50cf0296f553da2b34eea853aa7b
David Nowinsky <david@nowinsky.net>
2017-11-02 15:37:22 +0100

    November update

--- install.sh
+++ install.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
 
+DISTRO="$(lsb_release -s -c)"
+
 cd ~
+
+echo "*** Install basic packages..."
+sudo apt-get install -y curl git openssh-server vim zsh
+sudo apt-get install -y software-properties-common ca-certificates
 
 echo "*** Add repositories..."
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 MONGO_VERSION="3.4"
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/$MONGO_VERSION main" | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+NODE_VERSION="node_8.x"
+echo "deb https://deb.nodesource.com/$NODE_VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:moka/stable
 sudo add-apt-repository -y ppa:ne0sight/chrome-gnome-shell
 
-echo "*** Install basic packages..."
-sudo apt-get install -y curl git openssh-server vim zsh
-sudo apt-get install -y software-properties-common
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
 
 echo "*** Set default shell to zsh..."
 chsh -s $(which zsh)
@@ -28,7 +45,7 @@ echo "*** Install postgres..."
 sudo apt-get install -y postgresql
 
 echo "*** Install python packages..."
-sudo apt-get install -y python-dev python-pip python-setuptools
+sudo apt-get install -y python-dev python-pip python-setuptools virtualenv
 sudo pip install virtualenvwrapper
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
@@ -82,7 +99,7 @@ ln -sf ../.dotfiles/zsh/antigen.zsh .zsh/antigen.zsh
 ln -sf ../.dotfiles/zsh/pure.zsh .zsh/pure.zsh
 
 echo "*** Install node.js..."
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get remove -y cmdtest # Conflicts with yarn
 sudo apt-get install -y nodejs yarn
 
 echo "*** Install basic npm packages..."
@@ -98,6 +115,9 @@ sudo apt-get install oracle-java8-installer
 echo "*** Install virtualbox..."
 sudo apt-get install -y virtualbox
 
+echo "*** Install docker..."
+sudo apt-get install -y docker-ce
+
 echo "*** Install lastpass-cli..."
 sudo apt-get install -y openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip cmake build-essential pkg-config
 mkdir -p ~/apps
@@ -107,14 +127,22 @@ make
 sudo make install
 cd ~
 
-echo "*** Configure Gnome..."
-gsettings set org.gnome.desktop.wm.preferences focus-new-windows 'strict'
+echo "*** Install multimedia stuff..."
+sudo apt-get install -y spotify-client vlc
 
 echo "*** Install backgrounds icons and fonts ..."
 sudo apt-get install -y gnome-backgrounds gnome-themes-standard gtk2-engines-murrine moka-icon-theme ttf-ancient-fonts fonts-firacode
+git clone git@github.com:GNOME/gnome-backgrounds.git ~/Pictures/gnome-backgrounds
 
 echo "*** Install other web browsers..."
 sudo apt-get install -y chromium-browser epiphany
 
 echo "*** Install extensions..."
-sudo apt-get install chrome-gnome-shell
+sudo apt-get install -y chrome-gnome-shell
+
+echo "*** Install utilities..."
+sudo apt-get install -y filezilla gnome-tweak-tool htop
+
+echo "*** Configure..."
+gsettings set org.gnome.desktop.wm.preferences focus-new-windows 'strict'
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
